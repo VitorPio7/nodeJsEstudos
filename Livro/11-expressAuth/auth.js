7a49728b31edb9a26aac7ceb546246c986f61ad2
VitorPio7 <vitorvpio60@gmail.com>
2025-06-21 16:33:01 -0300

    today

--- Livro/11-expressAuth/auth.js
+++ Livro/11-expressAuth/auth.js
@@ -1,23 +1,48 @@
-import passport from 'passport';
-import expressSession from 'express-session';
-
+import passport from "passport";
+import expressSession from "express-session";
+import LocalStrategy from "passport-local";
+import { createHash } from "crypto";
+import { get } from "./user/model";
 export default function (app) {
-  passport.serializeUser((user, done) => done(null, user.username));
-  passport.deserializeUser((id, done) => {
-    const user = {
-      username: 'sspringer',
-      firstname: 'Sebastian',
-      lastname: 'Springer',
-    };
-    done(null, user);
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
+    const user = await get({ id });
+    if (!user) {
+      done("User not found");
+    } else {
+      done(null, user);
+    }
   });
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      /*converter a senha em um hash md5 */
+      const hash = createHash("md5").update(password).digest("hex");
+      const user = await get({ username, password: hash });
+      if (!user) {
+        done(null, false);
+      } else {
+        done(null, user);
+      }
+    })
+  );
   app.use(
     expressSession({
-      secret: 'top secret',
+      secret: "top secret",
       resave: false,
       saveUninitialized: false,
-    }),
+    })
   );
   app.use(passport.initialize());
   app.use(passport.session());
+  app.post(
+    "/login",
+    passport.authenticate("local", { failureRedirect: "/login.html" }),
+    (request, response) => {
+      response.redirect("/");
+    }
+  );
+  app.get("/logout", (request, response) => {
+    request.logout();
+    response.redirect("/");
+  });
 }
